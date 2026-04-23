c36f52d29e34ff9d61fcec55624dd631e8a7c6b4
Malthe Bøgelund Kristensen <makr55@elev.techcollege.dk>
2026-04-23 11:14:31 +0200

    D3 added properties and Unit enum

---
+++ Models/Product.cs
@@ -0,0 +1,17 @@
+namespace ErpCLI.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? ItemNumber { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public double? Price { get; set; }
+        public double? Cost { get; set; }
+        public string? Location { get; set; }
+    }
+    public enum Unit
+    {
+        Pieces, Hours, Meters
+    }
+}
