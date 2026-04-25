9c997f4b645f5199f7a0c3223192faa88e7683e6
TobiasAagaard <tach@trifork.com>
2026-04-25 23:42:30 +0200

    Refactor of Product file names and changed gitignore

--- Views/Company/CompanyListScreen.cs
+++ Views/Company/CompanyListScreen.cs
@@ -31,14 +31,13 @@ public class CompanyListScreen : Screen
         listPage.AddColumn("Land", nameof(Company.Country));
         listPage.AddColumn("Valuta", nameof(Company.Currency), 8);
 
-  
-        var companies = Database.Instance.GetAllCompanies();
+        List<Company> companies = Database.Instance.GetAllCompanies();
         foreach (Company model in companies) 
         {
             listPage.Add(model);
         }
 
-        var company = listPage.Select();
+        Company company = listPage.Select();
         if (company != null)
         {
             Screen.Display(new CompanyDetails(company));
