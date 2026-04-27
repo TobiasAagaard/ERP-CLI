fa70d3ff00e3abc77483cac1ae5e5880de894558
Malthe Bøgelund Kristensen <makr55@elev.techcollege.dk>
2026-04-27 13:56:28 +0200

    Added SalesDatabase.cs

---
+++ Data/SalesDatabase.cs
@@ -0,0 +1,52 @@
+using ErpCli.Models;
+
+namespace ErpCli.Data
+{
+    public partial class Database
+    {
+        List<SalesOrderHeader> SalesOrderHeaderList = new List<SalesOrderHeader>()
+        {
+            new SalesOrderHeader { OrderNumber = 1, OrderCreatedAt = new DateTime(2025, 12, 05), OrderCompletedAt = new DateTime(2025, 12, 06), CustomerId = 5, status = SalesOrderHeader.Status.Oprettet}
+        };
+        public SalesOrderHeader GetSalesOrderHeader(int id)
+        {
+            for (int i = 0; i < SalesOrderHeaderList.Count; i++)
+            {
+                SalesOrderHeader SalesOrderHeader = SalesOrderHeaderList[i];
+                if (SalesOrderHeader.OrderNumber == id)
+                    return SalesOrderHeader;
+            }
+            return null;
+        }
+        public List<SalesOrderHeader> GetSalesOrderHeaders()
+        {
+            List<SalesOrderHeader> SalesOrderHeaderCopy = new();
+            SalesOrderHeaderCopy.AddRange(SalesOrderHeaderList);
+            return SalesOrderHeaderCopy;
+        }
+        public void AddSalesOrderHeader(SalesOrderHeader SalesOrderHeader)
+        {
+            SalesOrderHeaderList.Add(SalesOrderHeader);
+        }
+        public void UpdateSalesOrderHeader(SalesOrderHeader editSalesOrderHeader)
+        {
+            for (int i = 0; i < SalesOrderHeaderList.Count; i++)
+            {
+                SalesOrderHeader SalesOrderHeader = SalesOrderHeaderList[i];
+                if (SalesOrderHeader.OrderNumber == editSalesOrderHeader.OrderNumber)
+                    SalesOrderHeaderList[i] = editSalesOrderHeader;
+            }
+        }
+        public void DeleteSalesOrderHeader(SalesOrderHeader SalesOrderHeader)
+        {
+            if(SalesOrderHeader.OrderNumber == 0)
+            {
+                return;
+            }
+            if (SalesOrderHeaderList.Contains(SalesOrderHeader))
+            {
+                SalesOrderHeaderList.Remove(SalesOrderHeader);
+            }
+        }
+    }
+}
