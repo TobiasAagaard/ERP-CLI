1ac395a7ccdaa748607bdcab7d64fd64264373ae
TobiasAagaard <tach@trifork.com>
2026-04-28 18:45:00 +0200

    Added first test, products. This when ProfitPercent calcs correctly

---
+++ ERP-CLI.Tests/ProductTests.cs
@@ -0,0 +1,20 @@
+﻿using ErpCli.Models;
+
+namespace ERP_CLI.Tests;
+
+public class ProductTests
+{
+    [Fact]
+    public void ProfitPercent_WhenPriceIsDoubleCost_Returns100()
+    {
+        Product product = new Product
+        {
+            Price = 100,
+            Cost = 50,
+        };
+
+        var result = product.ProfitPercent;
+
+        Assert.Equal(100, result);
+    }
+}
