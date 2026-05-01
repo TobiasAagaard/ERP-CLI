056d3b8d2398c28dc6985b964b70cbf3f15af50e
TobiasAagaard <tach@trifork.com>
2026-05-01 10:37:04 +0200

    Search util

--- Utils/Search.cs
+++ Utils/Search.cs
@@ -2,9 +2,23 @@ namespace ErpCli.Utils
 {
     public class Search
     {
-        public void SearchByParameter<T>(List<T> list, string searchParameter)
+        public void SearchByParameter<T>(List<T> list, string SearchPropety)
         {
-            Console.Write($"Søge efter {searchParameter}");
+            Console.Write($"Søge efter {SearchPropety}");
+            string SearchTerm = Console.ReadLine() ?? string.Empty;
+            List<T> SearchResults = list.Where(x => x.GetType().GetProperty(SearchPropety)?.GetValue(x)?.ToString() == SearchTerm).ToList();
+            if (SearchResults.Count > 0)
+            {
+                Console.WriteLine($"Fandt {SearchResults.Count} resultater:");
+                foreach (T result in SearchResults)
+                {
+                    Console.WriteLine(result);
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Ingen resultater fundet gå ind i  menu for at oprette ny .");
+            }
         }
     }
 }
